daf6cc1ecac888630ed7ccd73a2fc571c3c656ce
Danae Lyons <118503247+Danaelyons@users.noreply.github.com>
2023-01-26 15:07:17 -0800

    Added questions for my game.

--- game.js
+++ game.js
@@ -10,57 +10,57 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
+let questions0 = [
     {
-        question: 'What is a boolean?'
-        choice1: 'True or false'
-        choice2: 'A number'
-        choice3: 'An array'
-        choice4: 'My Aunt'
+        question: 'What is a boolean?',
+        choice1: 'True or false',
+        choice2: 'A number',
+        choice3: 'An array',
+        choice4: 'My Aunt',
         answer: 1,
     },
 ]
 
-let questions = [
+let questions1 = [
     {
-        question: 'What is does && in coding called?'
-        choice1: 'True or false'
-        choice2: 'A number'
-        choice3: 'A logical and'
-        choice4: 'My Uncle'
+        question: 'What is && in coding called?',
+        choice1: 'True or false',
+        choice2: 'A number',
+        choice3: 'A logical and',
+        choice4: 'My Uncle',
         answer: 3,
     },
 ]
 
-let questions = [
+let questions2 = [
     {
-        question: 'What does && mean?'
-        choice1: 'True or false'
-        choice2: 'A number'
-        choice3: 'An array'
-        choice4: 'both expressions must be true in order to log as true.'
-        answer: 4,
+        question: 'What does || mean?',
+        choice1: 'True or false',
+        choice2: 'one expression must be true in order to log as true.',
+        choice3: 'A string',
+        choice4: 'both expressions must be true in order to log as true.',
+        answer: 2,
     },
 ]
 
-let questions = [
+let questions3 = [
     {
-        question: 'What is a boolean?'
-        choice1: 'True or false'
-        choice2: 'A number'
-        choice3: 'An array'
-        choice4: 'My Aunt'
-        answer: 1,
+        question: 'What does == mean?',
+        choice1: 'False',
+        choice2: 'equal',
+        choice3: 'An array',
+        choice4: 'My dog',
+        answer: 2,
     },
 ]
 
-let questions = [
+let questions4 = [
     {
-        question: 'What is a boolean?'
-        choice1: 'True or false'
-        choice2: 'A number'
-        choice3: 'An array'
-        choice4: 'My Aunt'
-        answer: 1,
+        question: 'What is a string?',
+        choice1: 'True',
+        choice2: 'A question',
+        choice3: 'letters, numbers and punctuation',
+        choice4: 'False',
+        answer: 3,
     },
 ]
